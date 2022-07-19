87c6df1a68ce2efa7b118255ab61d0c7034c025d
sylvesterAdewale <adewalesylvesteromofemi28@gmail.com>
2022-07-19 20:37:24 +0100

    edited the teams page

--- pages/team.js
+++ pages/team.js
@@ -4,39 +4,47 @@ import Image from 'next/image'
 const Team = () => {
   return (
     <Layout>
-      <section>
-            <p>Our Team</p>
-            <p>The people behind the scenes.</p>
-            <div className="container-teams">
-            <div className="">
-              <div className="w-52 h-52 relative">
+      <section className="my-16">
+        <h1 className="trispace text-3xl lg:text-5xl uppercase font-black">Our Team</h1>
+        <p className="font-medium max-w-[800px] mt-2">The people behind the scenes.</p>
+        <div className="mt-8 flex flex-col mb-5 md:flex-row gap-5">
+            <div className="bg-orange-100 md:w-1/2 p-5 flex items-center font-medium">
+              <div className="w-28 h-28 relative">
                 <Image src="/assets/elijah.png" alt="mr-t image" layout="fill" objectFit="contain" />
               </div>
-                <div>
-                <p>Mr T </p>
+              <div className="flex-1">
+                <p className="trispace text-xl uppercase font-semibold">Mr T </p>
                 <p>Founder </p>
-                </div> 
+              </div> 
             </div>
-            <div>
-                <img src="/assets/pops.png" alt="pops image" />
-                <div>
-                <p>Pops</p>
+            <div className="bg-orange-100 md:w-1/2 p-5 flex items-center font-medium">
+              <div className="w-28 h-28 relative">
+                <Image src="/assets/tktkun.png" alt="mr-t image" layout="fill" objectFit="contain" />
+              </div>
+              <div className="flex-1">
+                <p className="trispace text-xl uppercase font-semibold">Pops</p>
                 <p>Co-founder</p>
-                </div>
+              </div> 
             </div>
-            <div>
-                <img src="/assets/tktkun.png" alt="tk-kun image" />
-                <div>
-                <p>Tk kun</p>
-                <p>	Project manager</p>
-                </div>
+          </div>
+          <div className="mb-8 flex flex-col md:flex-row gap-5">
+            <div className="bg-orange-100 md:w-1/2 p-5 flex items-center font-medium">
+              <div className="w-28 h-28 relative">
+                <Image src="/assets/nikama.png" alt="mr-t image" layout="fill" objectFit="contain" />
+              </div>
+              <div className="flex-1">
+                <p className="trispace text-xl uppercase font-semibold">Tk kun</p>
+                <p>Project manager</p>
+              </div> 
             </div>
-            <div>
-                <img src="/assets/nikama.png" alt="nakama image" />
-                <div>
-                <p>Nakama </p>
-                <p>	Project advisor </p>
-                </div>
+            <div className="bg-orange-100 md:w-1/2 p-5 flex items-center font-medium">
+              <div className="w-28 h-28 relative">
+                <Image src="/assets/elijah.png" alt="mr-t image" layout="fill" objectFit="contain" />
+              </div>
+              <div className="flex-1">
+                <p className="trispace text-xl uppercase font-semibold">Nakama</p>
+                <p>Project advisor</p>
+              </div> 
             </div>
             </div>
         </section>
